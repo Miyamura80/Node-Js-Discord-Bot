4120a33523e5ff0c62598515a527bcd0897543ee
Miyamura80 <eitomiyamura@gmail.com>
2020-07-06 16:54:02 +0100

    small commit for transfer 2

--- commands/assignshopitem.js
+++ commands/assignshopitem.js
@@ -30,6 +30,7 @@ module.exports = {
 		const itemDB = await Items.findOne({ where: { item_name: { [Op.like]: itemName } } });
 		if (!itemDB) return message.channel.send(`Sorry ${message.author}, that's an invalid item name.`);
 
+		
 		ShopListing.create({ user_id: transferTarget.id, balance: 0});
 		
 		//find shop-item pairing, to check for conflicts
